1588486c84582cbde6fdfad86ad6122b56931bdd
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2024-09-06 19:31:44 +0200

    interrupt pdf generation

--- src/server/index.ts
+++ src/server/index.ts
@@ -19,25 +19,41 @@ async function loadLocalFile() {
       "--allow-file-access-from-files",
       "--allow-file-access",
       "--allow-cross-origin-auth-prompt",
-      // "--window-size=1920,1080",
+      "--window-size=1920,1080",
     ],
-    defaultViewport: null,
-    // defaultViewport: {
-    //   width: 1920,
-    //   height: 1080,
-    // },
-  });
-  const page = await browser.newPage();
-  // Устанавливаем размеры страницы для A4
-  // await page.setViewport({ width: 2480, height: 3508 }); // Это A4 в пикселях при 300 DPI
-
-  await page.goto(filePath, {
-    waitUntil: "networkidle0", // Wait for all non-lazy loaded images to load
+    // defaultViewport: null,
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
   });
 
   http
     .createServer(async (_request, response) => {
       console.log("генерация pdf");
+      if (response.closed) {
+        console.log("Генерация прервана до открытия страницы");
+        return;
+      }
+      const page = await browser.newPage();
+      // Устанавливаем размеры страницы для A4
+      // await page.setViewport({ width: 2480, height: 3508 }); // Это A4 в пикселях при 300 DPI
+
+      if (response.closed) {
+        await page.close();
+        console.log("Генерация прервана после открытия страницы");
+        return;
+      }
+
+      await page.goto(filePath, {
+        waitUntil: "networkidle0", // Wait for all non-lazy loaded images to load
+      });
+
+      if (response.closed) {
+        await page.close();
+        console.log("Генерация прервана после открытия pdf");
+        return;
+      }
       await page.evaluate(async (data) => {
         console.log(data);
         // window.__FFM_PDF_DATA = {
@@ -48,16 +64,28 @@ async function loadLocalFile() {
         return window.__FFM_PDF_RUN_APP();
       }, TeamPageDataTest);
 
+      if (response.closed) {
+        await page.close();
+        console.log("Генерация прервана после рендеринга pdf");
+        return;
+      }
+
       const pdfStream = await page.createPDFStream({
         preferCSSPageSize: true,
         waitForFonts: true,
+        // tagged: true,
+        // format: "A4",
       });
 
       if (response.closed) {
         await pdfStream.cancel();
+        console.log("Генерация прервана после создания pdf");
       } else {
         await pdfStream.pipeTo(Writable.toWeb(response));
       }
+
+      await page.close();
+      console.log("генерация pdf завершена");
     })
     .on("close", () => {
       browser.close();
